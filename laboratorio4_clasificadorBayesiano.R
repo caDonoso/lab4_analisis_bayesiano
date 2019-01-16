12af0a1c59adaf80eaf9870156b5f4263ad9acf5
ReiSe <31135374+caDonoso@users.noreply.github.com>
2019-01-16 01:11:26 -0300

    Update laboratorio4_clasificadorBayesiano.R

--- laboratorio4_clasificadorBayesiano.R
+++ laboratorio4_clasificadorBayesiano.R
@@ -22,12 +22,92 @@ datos <- read.csv("D:/Universidad/Analisis de datos/Lab 4/agaricus-lepiota.data"
 #   - poisonous: 38.2% (2156)
 filtered.datos <- datos[datos$stalk.root != "?",]
 
-mushroom <- sample(2, nrow(filtered.datos), replace = TRUE, prob = c(0.62, 0.38))
-trainD <- filtered.datos[mushroom==1,]
-testD <- filtered.datos[mushroom==2,]
+#Se elimina el atributo veil-type ya que es una elemento de tipo constante por lo que no aporta al estudio.
+#Se mantiene el número total de datos, no hay variación absoluta en este.
+filtered.datos <- subset(filtered.datos, select = -c(veil.type))
 
-nrow(trainD)
-nrow(testD)
 
-conditionalProbabilities <- naiveBayes(filtered.datos ~ .,data=trainD)
-conditionalProbabilities
+columnas <- c("cap.shape","cap.surface","cap.color","bruises","odor","gill.attachment","gill.spacing","gill.size","gill.color"
+              ,"stalk.shape","stalk.root","stalk.surface.above.ring","stalk.surface.below.ring","stalk.color.above.ring"
+              ,"stalk.color.below.ring","veil.color","ring.number","ring.type","spore.print.color","population","habitat")
+
+
+#Conjunto de entrenamiento del 95%
+mush_test <- sample(1:nrow(filtered.datos), nrow(filtered.datos)*0.95)
+nb_mush <- naiveBayes(filtered.datos[-mush_test, columnas], filtered.datos[-mush_test, "class"])
+pred_mush <- predict(nb_mush, filtered.datos[mush_test, columnas])
+
+datosTabla <- filtered.datos[mush_test,]
+tabla1.c <- table(pred_mush,datosTabla$class)
+tabla1.c
+
+clasificadosCorrectamente <- sum(pred_mush==filtered.datos[mush_test, "class"])
+totalDatos <- nrow(filtered.datos)*0.95
+clasificadosCorrectamente
+totalDatos
+precision <- (clasificadosCorrectamente / totalDatos) * 100
+precision
+
+#Conjunto de entrenamiento del 80%
+mush_test <- sample(1:nrow(filtered.datos), nrow(filtered.datos)*0.8)
+nb_mush <- naiveBayes(filtered.datos[-mush_test, columnas], filtered.datos[-mush_test, "class"])
+pred_mush <- predict(nb_mush, filtered.datos[mush_test, columnas])
+
+datosTabla <- filtered.datos[mush_test,]
+tabla1.c <- table(pred_mush,datosTabla$class)
+tabla1.c
+
+clasificadosCorrectamente <- sum(pred_mush==filtered.datos[mush_test, "class"])
+totalDatos <- nrow(filtered.datos)*0.8
+clasificadosCorrectamente
+totalDatos
+precision <- (clasificadosCorrectamente / totalDatos) * 100
+precision
+
+#Conjunto de entrenamiento del 60%
+mush_test <- sample(1:nrow(filtered.datos), nrow(filtered.datos)*0.6)
+nb_mush <- naiveBayes(filtered.datos[-mush_test, columnas], filtered.datos[-mush_test, "class"])
+pred_mush <- predict(nb_mush, filtered.datos[mush_test, columnas])
+
+datosTabla <- filtered.datos[mush_test,]
+tabla1.c <- table(pred_mush,datosTabla$class)
+tabla1.c
+
+clasificadosCorrectamente <- sum(pred_mush==filtered.datos[mush_test, "class"])
+totalDatos <- nrow(filtered.datos)*0.6
+clasificadosCorrectamente
+totalDatos
+precision <- (clasificadosCorrectamente / totalDatos) * 100
+precision
+
+#Conjunto de entrenamiento del 40%
+mush_test <- sample(1:nrow(filtered.datos), nrow(filtered.datos)*0.4)
+nb_mush <- naiveBayes(filtered.datos[-mush_test, columnas], filtered.datos[-mush_test, "class"])
+pred_mush <- predict(nb_mush, filtered.datos[mush_test, columnas])
+
+datosTabla <- filtered.datos[mush_test,]
+tabla1.c <- table(pred_mush,datosTabla$class)
+tabla1.c
+
+clasificadosCorrectamente <- sum(pred_mush==filtered.datos[mush_test, "class"])
+totalDatos <- nrow(filtered.datos)*0.4
+clasificadosCorrectamente
+totalDatos
+precision <- (clasificadosCorrectamente / totalDatos) * 100
+precision
+
+#Conjunto de entrenamiento del 20%
+mush_test <- sample(1:nrow(filtered.datos), nrow(filtered.datos)*0.2)
+nb_mush <- naiveBayes(filtered.datos[-mush_test, columnas], filtered.datos[-mush_test, "class"])
+pred_mush <- predict(nb_mush, filtered.datos[mush_test, columnas])
+
+datosTabla <- filtered.datos[mush_test,]
+tabla1.c <- table(pred_mush,datosTabla$class)
+tabla1.c
+
+clasificadosCorrectamente <- sum(pred_mush==filtered.datos[mush_test, "class"])
+totalDatos <- nrow(filtered.datos)*0.2
+clasificadosCorrectamente
+totalDatos
+precision <- (clasificadosCorrectamente / totalDatos) * 100
+precision
